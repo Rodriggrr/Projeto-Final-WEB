dd551a0a3fd59a1bce3021dd94e87fc73a747b75
Rodrigo Farinon <rfarinon@alu.ufc.br>
2025-01-25 15:48:57 -0300

    melhora no desempenho

--- src/nav/add_navbar.js
+++ src/nav/add_navbar.js
@@ -2,14 +2,10 @@ window.onload = function() {
     document.body.classList.add('loaded');
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-    fetch("../src/nav/navbar.html")
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById("navbar").innerHTML = data;
-        })
-        .catch(error => console.error('Erro ao carregar o navbar:', error));
-});
-
-
+fetch("../src/nav/navbar.html")
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("navbar").innerHTML = data;
+    })
+    .catch(error => console.error('Erro ao carregar o navbar:', error));
 
