86e90e6b1d6fcd97c650c75396e1188bcdd0cfbf
Rodrigo Farinon <rfarinon@alu.ufc.br>
2025-01-24 22:40:01 -0300

    up

--- src/nav/add_navbar.js
+++ src/nav/add_navbar.js
@@ -11,3 +11,18 @@ document.addEventListener("DOMContentLoaded", () => {
         .catch(error => console.error('Erro ao carregar o navbar:', error));
 });
 
+document.addEventListener("mousemove", (event) => {
+    let body = document.body; // Pega diretamente o elemento <body>
+    let width = window.innerWidth;
+    let height = window.innerHeight;
+    let mouseX = event.clientX;
+    let mouseY = event.clientY;
+
+    const red = Math.round((mouseX / width) * 255);
+    const green = Math.round((mouseY / height) * 255);
+    const blue = 255 - red;
+
+    body.style.color = `rgb(${red}, ${green}, ${blue})`; // Aplica a cor dinamicamente
+});
+
+
