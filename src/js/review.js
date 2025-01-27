3b6a4ee0263fac141fc165ae99403f1f7afdca46
Rodrigo Farinon <rfarinon@alu.ufc.br>
2025-01-26 21:15:10 -0300

    up

---
+++ src/js/review.js
@@ -0,0 +1,33 @@
+let avaliacao_container = document.getElementsByClassName('avaliacao');
+let valores = document.getElementsByClassName('valor');
+
+function put_stars(container, value) {
+    value = (value < 0) ? 0 : value;
+    value = (value > 5) ? 5 : value;
+    let aux = (!isNaN(value)) ? value : 0;
+    let text = container.innerHTML;
+    container.innerHTML = ''; 
+    while (value > 0) {
+        if (Math.round(value) < 1) {
+            container.innerHTML += `<i class="bi bi-star-half"></i>`; 
+        } else {
+            container.innerHTML += `<i class="bi bi-star-fill"></i>`; 
+        }
+        value--; 
+    }
+    for(let i = 0; i < (5 - Math.ceil(aux)); i++)
+        container.innerHTML += `<i class="bi bi-star"></i>`; 
+    container.innerHTML += `${text}`;
+}
+
+document.addEventListener("ReviewSTARS", () => {
+    for (let i = 0; i < avaliacao_container.length; i++) {
+        const valor = parseFloat(valores[i].innerText);
+        console.log(`Valor para o container ${i}:`, valor);
+        put_stars(avaliacao_container[i], valor);
+    }
+})
+
+const evento = new Event("ReviewSTARS");
+document.dispatchEvent(evento);
+
