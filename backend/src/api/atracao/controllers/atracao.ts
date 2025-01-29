d4a08030fa5e18234fa3f1390b044d2505dd67a2
Rodrigo Farinon <rfarinon@alu.ufc.br>
2025-01-29 20:47:17 -0300

    strapi added and refactored directories

---
+++ backend/src/api/atracao/controllers/atracao.ts
@@ -0,0 +1,7 @@
+/**
+ * atracao controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+export default factories.createCoreController('api::atracao.atracao');
---
+++ backend/src/api/atracao/services/atracao.ts
@@ -0,0 +1,7 @@
+/**
+ * atracao service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::atracao.atracao');
