1c58cafbd3c390792ebffe31e4cd8af4541089a1
Rodrigo Farinon <rfarinon@alu.ufc.br>
2025-02-02 13:09:14 -0300

    ajeitei o strapi para rodar em qualquer maquina

---
+++ backend/dist/src/api/avaliacao/services/avaliacao.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * avaliacao service
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreService('api::avaliacao.avaliacao');
