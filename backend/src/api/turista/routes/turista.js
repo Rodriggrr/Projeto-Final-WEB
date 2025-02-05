a7e1d0413a8321fed435c9567a7e954468dbb0d2
Rodrigo Farinon <rfarinon@alu.ufc.br>
2025-02-04 23:00:54 -0300

    atualizamos o db com 1 entry

---
+++ backend/src/api/turista/routes/turista.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * turista router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::turista.turista');
---
+++ backend/src/api/turista/services/turista.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * turista service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::turista.turista');
