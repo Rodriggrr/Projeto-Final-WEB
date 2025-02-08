f1a2329752662d7e210702b483fcf6321de6c293
Rodrigo Farinon <rfarinon@alu.ufc.br>
2025-02-08 14:14:13 -0300

    personalizei mais o update e delete

--- frontend/src/popup/add_popup.js
+++ frontend/src/popup/add_popup.js
@@ -65,61 +65,63 @@ function avaliarButton(user = true, button = '') {
     let id_avaliacao = '';
     popup = new Popup();
     popup.whenReady().then(() => {
+
+        fetch(`${API_URL}/users/me?populate[usuario][populate][reviews][populate][0]=avaliando_usuario`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${sessionStorage.getItem('jwtToken')}`
+            }
+        }).then(response => {
+            if (!response.ok) throw new Error('Erro ao carregar dados.');
+            return response.json();
+        }
+        ).then(data => {
+            console.log(data);
+            let avaliado = data.usuario.reviews;
+            for(let i = 0; i < avaliado.length; i++){
+                console.log(avaliado[i].avaliando_usuario.documentId + ' ' + avaliando_a);
+                console.log(avaliado);
+                if(avaliado[i].avaliando_usuario.documentId == avaliando_a){
+                    button.innerHTML = 'Atualizar avaliação';
+                    update = true;
+                    id_avaliacao = avaliado[i].documentId;
+                    document.getElementById('avaliar-number').value = avaliado[i].nota;
+                    document.getElementById('avaliar-textarea').value = avaliado[i].descricao;
+                    document.getElementById('avaliar-button').innerHTML = 'Atualizar';
+                    let deletarButton = document.getElementById('deletar-button');
+                    deletarButton.style.display = (update) ? 'inline' : 'none';
+                        deletarButton.addEventListener('click', () => {
+                            if (!confirm('Deseja realmente deletar essa avaliação?')) return;
+                            fetch(`${API_URL}/avaliacaos/${id_avaliacao}`, {
+                                method: 'DELETE',
+                                headers: {
+                                    'Content-Type': 'application/json',
+                                    'Authorization': `Bearer ${sessionStorage.getItem('jwtToken')}`
+                                }
+                            }).then(response => {
+                                if (!response.ok) throw new Error('Erro ao deletar avaliação.');
+                                return response.text();
+                            }).then(text => {
+                                if (text) {
+                                    const data = JSON.parse(text);
+                                    console.log(data);
+                                }
+                                window.location.reload();
+                            }).catch(error => {
+                                console.error(error);
+                            });
+                        });
+                    break;
+                }
+            }
+        });
+
         let avaliarButton = document.getElementById('avaliar-button');
         if (!id_search) button.style.display = 'none';
 
         button.addEventListener('click', () => {
             document.getElementById('popup-container').style.display = 'flex';
-
-            fetch(`${API_URL}/users/me?populate[usuario][populate][reviews][populate][0]=avaliando_usuario`, {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                    'Authorization': `Bearer ${sessionStorage.getItem('jwtToken')}`
-                }
-            }).then(response => {
-                if (!response.ok) throw new Error('Erro ao carregar dados.');
-                return response.json();
-            }
-            ).then(data => {
-                console.log(data);
-                let avaliado = data.usuario.reviews;
-                for(let i = 0; i < avaliado.length; i++){
-                    console.log(avaliado[i].avaliando_usuario.documentId + ' ' + avaliando_a);
-                    console.log(avaliado);
-                    if(avaliado[i].avaliando_usuario.documentId == avaliando_a){
-                        update = true;
-                        id_avaliacao = avaliado[i].documentId;
-                        document.getElementById('avaliar-number').value = avaliado[i].nota;
-                        document.getElementById('avaliar-textarea').value = avaliado[i].descricao;
-                        document.getElementById('avaliar-button').innerHTML = 'Atualizar';
-                        let deletarButton = document.getElementById('deletar-button');
-                        deletarButton.style.display = (update) ? 'inline' : 'none';
-                            deletarButton.addEventListener('click', () => {
-                                if (!confirm('Deseja realmente deletar essa avaliação?')) return;
-                                fetch(`${API_URL}/avaliacaos/${id_avaliacao}`, {
-                                    method: 'DELETE',
-                                    headers: {
-                                        'Content-Type': 'application/json',
-                                        'Authorization': `Bearer ${sessionStorage.getItem('jwtToken')}`
-                                    }
-                                }).then(response => {
-                                    if (!response.ok) throw new Error('Erro ao deletar avaliação.');
-                                    return response.text();
-                                }).then(text => {
-                                    if (text) {
-                                        const data = JSON.parse(text);
-                                        console.log(data);
-                                    }
-                                    window.location.reload();
-                                }).catch(error => {
-                                    console.error(error);
-                                });
-                            });
-                        break;
-                    }
-                }
-            });
         });
 
         avaliarButton.addEventListener('click', () => {
