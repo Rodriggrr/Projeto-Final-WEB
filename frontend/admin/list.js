aabec4e5f05c656311a86156f30d46d6f167f002
Rodrigo Farinon <rfarinon@alu.ufc.br>
2025-02-23 07:36:19 -0300

    admin

---
+++ frontend/admin/list.js
@@ -0,0 +1,95 @@
+let params = new URLSearchParams(window.location.search);
+let type = params.get('type');
+if(!type) { type = 'dashboard'; window.location.href = `./index.html`; }
+
+URL = `http://localhost:1337/api/${type}`;
+
+async function getItems(type) {
+    const res = await fetch(`${URL}`);
+    return await res.json();
+}
+
+async function deleteItem(type, id) {
+    const res = await fetch(`${URL}/${id}`, {
+        method: 'DELETE',
+        headers: {
+            'Authorization': `Bearer ${sessionStorage.getItem('jwtToken')}`
+        }
+    });
+    return await res.json();
+}
+
+async function updateItem(type, id, data) {
+    const res = await fetch(`${URL}/${id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${sessionStorage.getItem('jwtToken')}`
+        },
+        body: JSON.stringify(data)
+    });
+    return await res.json();
+}
+
+async function createItem(type, data) {
+    const res = await fetch(`${URL}`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${sessionStorage.getItem('jwtToken')}`
+        },
+        body: JSON.stringify(data)
+    });
+    return await res.json();
+}
+
+function populateMain(main) {
+    getItems(type)
+        .then(async (data) => {
+            const items = data.data;
+            console.log('Items:', items);
+
+            let table = document.querySelector('table');
+            let tbody = table.querySelector('tbody');
+            for (let i = 0; i < items.length; i++) {
+                let item = items[i];
+                let row = document.createElement('tr');
+                row.innerHTML = `
+                <td class=id>${item.id}</td>
+                <td>${item.documentId}</td>
+                <td>${item.nome}</td>
+                <td class=nota>${await getMediaNota(item.documentId)}</td>
+                <td class="actions">
+                    <button class="edit">Editar</button>
+                    <button class="delete">Excluir</button>
+                </td>
+                `;
+                tbody.appendChild(row);
+            }
+        }).then(() => {
+            let rows = document.querySelectorAll('tbody tr');
+            rows.forEach(row => {
+                let id = row.querySelector('.id').textContent;
+                let editButton = row.querySelector('.edit');
+                let deleteButton = row.querySelector('.delete');
+                editButton.addEventListener('click', () => {
+                    window.location.href = `./edit.html?type=${type}&id=${id}`;
+                });
+                deleteButton.addEventListener('click', () => {
+                    deleteItem(type, id).then(data => {
+                        console.log('Item deletado:', data);
+                        window.location.reload();
+                    });
+                });
+            });
+        }).catch(error => {
+            console.error('Erro:', error);
+            main.innerHTML = "Erro ao carregar os itens.";
+        });
+
+    }
+
+
+
+let main = document.getElementsByTagName('main')[0];
+populateMain(main);
